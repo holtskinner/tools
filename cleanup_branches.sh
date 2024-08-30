6d343567d88e23ab9a9f1bece527f31f2b85fbf3
Holt Skinner <holtskinner@google.com>
2024-08-30 11:58:34 -0500

    Update branch cleanup

--- cleanup_branches.sh
+++ cleanup_branches.sh
@@ -8,12 +8,12 @@ local_branches=$(git branch -vv | grep ": gone]" | awk '{print $1}')
 
 # Iterate through local branches and delete those without remotes
 for branch in $local_branches; do
-    remote=$(git config --get branch.$branch.remote)
-    
+    remote=$(git config --get "branch.$branch.remote")
+
     if [ -z "$remote" ]; then
         # If remote is empty, branch doesn't have a remote counterpart
         echo "Deleting local branch: $branch"
-        git branch -d $branch
+        git branch -d "$branch"
     fi
 done
 
